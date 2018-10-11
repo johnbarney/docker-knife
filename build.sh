2042adc713032eb29c7a27a2321bed1358db10a8
John Barney <barneyjo@amazon.com>
2018-10-11 13:15:37 -0700

    fix if statements again

--- build.sh
+++ build.sh
@@ -5,12 +5,18 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 
 cd ${BASE}/${VERSION}
 
-if [[ $VERSION == "latest" && $BASE == "ruby" ]]; then
-    DOCKER_TAG=latest
-elif [[ $VERSION == "latest" && $BASE == "alpine" ]]; then
-    DOCKER_TAG=alpine
+if [ $BASE == "ruby" ]; then
+    if [ $VERSION == "latest" ]; then
+        DOCKER_TAG = latest
+    else
+        DOCKER_TAG = $VERSION
+    fi
 else
-    DOCKER_TAG=${VERSION}-${BASE}
+    if [ $VERSION == "latest" ]; then
+        DOCKER_TAG = alpine
+    else
+        DOCKER_TAG = ${VERSION}-${BASE}
+    fi
 fi
 
 docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .

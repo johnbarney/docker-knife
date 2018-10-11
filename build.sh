0d0615ec3b2c8fc1deabd1d71d6fdaedc0c0a358
John Barney <barneyjo@amazon.com>
2018-10-11 13:04:56 -0700

    fix tagging

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 
 cd ${BASE}/${VERSION}
 
-if ${VERSION} == "latest" && ${BASE} == "ruby"; then
+if $VERSION == "latest" && $BASE == "ruby"; then
     DOCKER_TAG=latest
-elif ${VERSION} == "latest" && ${BASE} == "alpine"; then
+elif $VERSION == "latest" && $BASE == "alpine"; then
     DOCKER_TAG=alpine
 else
     DOCKER_TAG=${VERSION}-${BASE}
 fi
 
 docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
+docker push ${DOCKER_IMAGE}:${DOCKER_TAG}

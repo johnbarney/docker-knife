5400c61659609cd832bb4a053fa7ada6d9176803
John Barney <barneyjo@amazon.com>
2018-10-11 12:46:13 -0700

    fix alpine and latest tags

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 
 cd ${BASE}/${VERSION}
 
-if ${VERSION} == "latest"; then
+if ${VERSION} == "latest" && ${BASE} == "ruby"; then
     DOCKER_TAG=latest
+elif ${VERSION} == "latest" && ${BASE} == "alpine"; then
+    DOCKER_TAG=alpine
 else
     DOCKER_TAG=${VERSION}-${BASE}
 fi

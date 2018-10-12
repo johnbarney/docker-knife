2fde680cff0470fbb86185cd0e8868381d83f775
John Barney <barneyjo@amazon.com>
2018-10-12 00:27:28 -0700

    matrix failure

--- ci.sh
+++ ci.sh
@@ -17,22 +17,9 @@ else
     fi
 fi
 
-build() {
-    docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
-}
+# Build container
+docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
 
-push() {
-    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-    docker push ${DOCKER_IMAGE}:${DOCKER_TAG}
-}
-
-case "$1" in
-  build)
-    build
-    ;;
-  push)
-    push
-    ;;
-  *)
-    echo "Usage: $0 {build|push}"
-esac
+# Push container
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push ${DOCKER_IMAGE}:${DOCKER_TAG}

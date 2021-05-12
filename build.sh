3a6a0202296923d9315c13b6891621b40b024eb8
John Barney <john.barney@johnbarney.co>
2021-05-12 13:50:09 +0800

    Pushing builds is useful

--- build.sh
+++ build.sh
@@ -26,10 +26,10 @@ else
 fi
 
 # DockerHub Login
-#echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 # Build container
-docker build --build-arg VERSION="${GEM_VERSION}" --build-arg GEM="${GEM}" -t "${DOCKER_IMAGE}":"${DOCKER_TAG}" .
+docker build --build-arg TAG=${TAG} --build-arg VERSION="${GEM_VERSION}" --build-arg GEM="${GEM}" -t "${DOCKER_IMAGE}":"${DOCKER_TAG}" .
 
 # Push container
-#docker push "${DOCKER_IMAGE}":"${DOCKER_TAG}"
+docker push "${DOCKER_IMAGE}":"${DOCKER_TAG}"

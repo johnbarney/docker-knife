5479415448a30b4b495f7bdbe0a24c7e9aac7c13
John Barney <john.barney@johnbarney.co>
2021-05-12 13:43:35 +0800

    further optimizations

--- build.sh
+++ build.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 DOCKER_IMAGE="${DOCKER_USERNAME}"/docker-knife
+GEM_VERSION="--version ~>${VERSION}"
+GEM=knife
 
 cd "${BASE}"
 
 # Are we building Knife 17 or newer?
 if [ "${VERSION}" != "latest" ]; then
   if [ "${VERSION}" -le 16 ]; then
-    cd legacy
-    CHEF_VERSION="--version ~>${VERSION}"
-    LEGACY_BUILD=1
-  else
-    KNIFE_VERSION="--version ~>${VERSION}"
+    GEM=chef
   fi
+else
+  GEM_VERSION=""
 fi
 
 # Set tags
 if [ "${BASE}" == "ruby" ]; then
-    DOCKER_TAG="${VERSION}"
+  DOCKER_TAG="${VERSION}"
 else
-    if [ "${VERSION}" == "latest" ]; then
-        DOCKER_TAG=alpine
-    else
-        DOCKER_TAG="${VERSION}"-"${BASE}"
-    fi
+  if [ "${VERSION}" == "latest" ]; then
+    DOCKER_TAG=alpine
+  else
+    DOCKER_TAG="${VERSION}"-"${BASE}"
+  fi
 fi
 
 # DockerHub Login
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+#echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 # Build container
-if [ "${LEGACY_BUILD}" -eq 1 ]; then
-  docker build --build-arg CHEF_VERSION="${CHEF_VERSION}" -t "${DOCKER_IMAGE}":"${DOCKER_TAG}" .
-else
-  docker build --build-arg KNIFE_VERSION="${KNIFE_VERSION}" -t "${DOCKER_IMAGE}":"${DOCKER_TAG}" .
-fi
+docker build --build-arg VERSION="${GEM_VERSION}" --build-arg GEM="${GEM}" -t "${DOCKER_IMAGE}":"${DOCKER_TAG}" .
 
 # Push container
-docker push "${DOCKER_IMAGE}":"${DOCKER_TAG}"
+#docker push "${DOCKER_IMAGE}":"${DOCKER_TAG}"

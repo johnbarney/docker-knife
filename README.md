0854ab37cd5c511d9962e75058bd6ab1cec3289c
John Barney <john.barney@johnbarney.co>
2021-05-12 11:44:22 +0800

    EOL Chef 12-14, Support for Chef 17+, make build script safer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## docker-knife [![Build Status](https://travis-ci.com/johnbarney/docker-knife.svg?branch=master)](https://travis-ci.com/johnbarney/docker-knife)
+## Docker-Knife [![Build Status](https://travis-ci.com/johnbarney/docker-knife.svg?branch=master)](https://travis-ci.com/johnbarney/docker-knife)
 
 [Docker Hub](https://hub.docker.com/r/johnbarney/docker-knife/)
 
@@ -6,6 +6,12 @@ This container is in response to not wanting a flood of gem sets or the omnibus 
 
 Example usage:
 
-`docker run -v $(pwd):/knife johnbarney/docker-knife:12 bootstrap node07`
+`docker run -v $(pwd):/knife johnbarney/docker-knife:16 bootstrap node07`
 
 If you are in your chef-repo, this will mount your chef-repo as the working directory. This assumes your knife.rb file is within your chef-repo folder and not in /etc/chef or some other location.
+
+## Change Log
+
+11-5-2021
+* Removed support for EOL knife versions (12-14)
+* Added support for Knife 17+
--- build.sh
+++ build.sh
@@ -1,15 +1,27 @@
-#!/usr/bin/env bash
-DOCKER_IMAGE=${DOCKER_USERNAME}/docker-knife
+#!/bin/bash
+DOCKER_IMAGE="${DOCKER_USERNAME}"/docker-knife
 
-cd ${BASE}/${VERSION}
+cd "${BASE}"
 
-if [ $BASE == "ruby" ]; then
-    DOCKER_TAG=$VERSION
+# Are we building Knife 17 or newer?
+if [ "${VERSION}" != "latest" ]; then
+  if [ "${VERSION}" -le 16 ]; then
+    cd legacy
+    CHEF_VERSION="--version ~>${VERSION}"
+    LEGACY_BUILD=1
+  else
+    KNIFE_VERSION="--version ~>${VERSION}"
+  fi
+fi
+
+# Set tags
+if [ "${BASE}" == "ruby" ]; then
+    DOCKER_TAG="${VERSION}"
 else
-    if [ $VERSION == "latest" ]; then
+    if [ "${VERSION}" == "latest" ]; then
         DOCKER_TAG=alpine
     else
-        DOCKER_TAG=${VERSION}-${BASE}
+        DOCKER_TAG="${VERSION}"-"${BASE}"
     fi
 fi
 
@@ -17,7 +29,11 @@ fi
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 # Build container
-docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
+if [ "${LEGACY_BUILD}" -eq 1 ]; then
+  docker build --build-arg CHEF_VERSION="${CHEF_VERSION}" -t "${DOCKER_IMAGE}":"${DOCKER_TAG}" .
+else
+  docker build --build-arg KNIFE_VERSION="${KNIFE_VERSION}" -t "${DOCKER_IMAGE}":"${DOCKER_TAG}" .
+fi
 
 # Push container
-docker push ${DOCKER_IMAGE}:${DOCKER_TAG}
+docker push "${DOCKER_IMAGE}":"${DOCKER_TAG}"
